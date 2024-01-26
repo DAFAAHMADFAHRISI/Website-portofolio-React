1d053d3aa7983853425f9b2dd4e77a7b00fc4c79
Ahdiat Brafiadi <111393545+brafiadi@users.noreply.github.com>
2024-01-27 01:47:55 +0700

    up

--- src/pages/home/awal.jsx
+++ src/pages/home/awal.jsx
@@ -2,7 +2,7 @@ function Awal() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1 className="text-red-300"> halo guys</h1>
+      <h1 className="text-red-300">halo guys</h1>
     </>
   );
 }
